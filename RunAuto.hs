7cc9e64fb23807de3bf258d3aa6dd7d52f5f6cd6
wojtek <wojtek@example.com>
2017-03-24 19:06:06 +0100

    mostly finished

--- RunAuto.hs
+++ RunAuto.hs
@@ -1,64 +1,87 @@
 import System.Environment
 import System.Exit
 import Text.Read
+import Control.Monad
 
 import Auto
 
+
+
 newtype Alpha = Alpha Char deriving (Eq)
+
+
 
 instance Bounded Alpha where
     minBound = Alpha 'A'
     maxBound = Alpha 'Z'
 
+
+
 instance Enum Alpha where
     fromEnum (Alpha c) = fromEnum c
     toEnum n = Alpha (toEnum n)
+
+
 
 instance Show Alpha where
     show (Alpha c) = show c
 
 
-main = getArgs >>= parse >>= putStr . tac
 
-tac :: String -> String
-tac  = unlines . reverse . lines
+main :: IO ()
+main = getArgs >>= parse >>= putStrLn . check
 
 
-parse fs = concat `fmap` mapM readFile fs
 
-check :: Maybe (Auto Alpha Int) -> Maybe [Alpha] -> Maybe Bool
-check (Just aut) (Just s) = Just (accepts aut s)
-check _ _ = Nothing
+parse :: [FilePath] -> IO String
+parse [fname] = readFile fname
+parse _ = (putStrLn "Wrong number of params.") >> (exitWith ExitSuccess)
 
-buildAuto :: [String] -> Maybe (Auto Alpha Int)
-buildAuto (n:init:acc:tr) = 
-    fromListsM (readMaybe n) (readMaybe init) (readMaybe acc) (parseTr tr)
 
-fromListsM :: Maybe Int -> 
-              Maybe [Int] -> 
-              Maybe [Int] ->
-              Maybe [(Int,Alpha,[Int])] -> Maybe (Auto Alpha Int)
 
-fromListsM _ _ _ _ = Nothing
+check :: String -> String
+check s = if length (lines s) >= 4 then
+              case liftM2 accepts parsedAuto parsedWord of
+                  Just b -> show b
+                  Nothing -> "BAD INPUT"
+          else "BAD INPUT"
+          where parsedAuto = parseAuto (init l)
+                parsedWord = mapM readAlphaM (last l)
+                l = lines s
+
+
+
+parseAuto :: [String] -> Maybe (Auto Alpha Int)
+parseAuto (n:init:acc:tr) = liftM4 fromLists (liftM gen (readMaybe n))
+                                             (readMaybe init :: Maybe [Int]) 
+                                             (readMaybe acc :: Maybe [Int]) 
+                                             (parseTr tr)
+parseAuto _ = Nothing
+
+
+
+gen :: Int -> [Int]
+gen n = [1..n]
+
+
 
 parseTr :: [String] -> Maybe [(Int,Alpha,[Int])]
-parseTr _ = Nothing
-
-parseTrRec :: [[String] -> Maybe [(Int,Alpha,[Int])] -> Maybe [(Int,Alpha,[Int])]
+parseTr = fmap concat . (mapM (parseSingleTr . words))
 
 
-parseSingleTr
 
- {-
-fromParts :: [String] -> Maybe (Auto Alpha Int, String)                                                                                                       
-fromParts (n:init:acc:rest) = (fromListsM (readMaybe init) [] [] [], "")
-fromParts _ = Nothing
- 
-fromListsM :: Maybe [Int] -> [Int] -> [Int] -> [(Int,Alpha,[Int])] -> Maybe (Auto Alpha Int) 
-fromListsM (Just a) b c d = Just (fromLists a b c d)
-fromListsM _ _ _ _ = Nothing
+parseSingleTr :: [String] -> Maybe [(Int,Alpha,[Int])]
+parseSingleTr (q:w:qs) = liftM3 buildSingleTr (readMaybe q) 
+                                              (mapM readAlphaM w) 
+                                              (mapM readMaybe qs)
+parseSingleTr _ = Nothing
 
 
- fromParts (s:ns:un:db:ud:pr:[])                                                                                                                    
- 76     = newSessionM (readMaybe s) (readMaybe ns) (Just un) (readMaybe db) (readMaybe ud) (readMaybe pr)                                              
- 77 fromParts _ = Nothing   -}
+
+buildSingleTr :: Int -> [Alpha] -> [Int] -> [(Int,Alpha,[Int])]
+buildSingleTr q chars qs = [(q,c,qs) | c <- chars]
+
+
+
+readAlphaM :: Char -> Maybe Alpha
+readAlphaM c = if c < 'A' || c > 'Z' then Nothing else Just (Alpha c)
